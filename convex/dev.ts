d1d78f457ffc322198189690705e341151f8fbdf
muzzlol <muzxmmilkhxn@gmail.com>
2025-11-16 23:32:06 +0530

    rm -rf tables cmd

---
+++ convex/dev.ts
@@ -0,0 +1,35 @@
+import type { TableNames } from "./_generated/dataModel"
+import { mutation } from "./_generated/server"
+
+export const clearAllTables = mutation({
+  handler: async (ctx) => {
+    const tables = [
+      "authAccounts",
+      "authRateLimits",
+      "authRefreshTokens",
+      "authSessions",
+      "authVerificationCodes",
+      "authVerifiers",
+      "users",
+      "numbers"
+    ] as const
+
+    type Tables = (typeof tables)[number]
+
+    type IsExact = Tables extends TableNames
+      ? TableNames extends Tables
+        ? true
+        : false
+      : false
+
+    type Check = IsExact
+    for (const tableName of tables) {
+      const docs = await ctx.db.query(tableName as TableNames).collect()
+      for (const doc of docs) {
+        await ctx.db.delete(doc._id)
+      }
+    }
+
+    return { clearedTables: tables, message: "All data cleared" }
+  }
+})
