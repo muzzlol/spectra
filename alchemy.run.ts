d034f599be4525b344a82062b305243dc8d5a258
muzzlol <muzxmmilkhxn@gmail.com>
2025-12-13 18:17:58 +0530

    add alchemy--github ci

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,7 +1,11 @@
 import alchemy from "alchemy"
 import { DurableObjectNamespace, TanStackStart } from "alchemy/cloudflare"
+import { GitHubComment } from "alchemy/github"
+import { CloudflareStateStore } from "alchemy/state"
 
-const app = await alchemy("spectra")
+const app = await alchemy("spectra", {
+  stateStore: (scope) => new CloudflareStateStore(scope)
+})
 
 export const arenas = await DurableObjectNamespace("ARENAS", {
   className: "ArenaWSS",
@@ -16,8 +20,27 @@ export const worker = await TanStackStart("WORKER", {
     ARENAS: arenas.id
   }
 })
+
 console.log({
   url: worker.url
 })
+
+if (process.env.PULL_REQUEST) {
+  await GitHubComment("preview-comment", {
+    owner: "muzzlol",
+    repository: "spectra",
+    issueNumber: Number(process.env.PULL_REQUEST),
+    body: `## [Preview Deployed]
+
+Your changes have been deployed to a preview environment:
+
+**[Website]:** ${worker.url}
+
+Built from commit ${process.env.GITHUB_SHA?.slice(0, 7)}
+
++---
+`
+  })
+}
 
 await app.finalize()
